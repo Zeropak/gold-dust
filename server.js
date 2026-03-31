02c5d752ccf64c68113d716b91e7970942f13fa5
Zeropak <46861973+Zeropak@users.noreply.github.com>
2026-03-31 22:30:14 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const http = require("http");
 
+const PORT = process.env.PORT || 3000;
+
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
+  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
   res.end("<h1>Мой сервер работает 🚀</h1>");
 });
 
-server.listen(3000, () => {
-  console.log("Server started");
+server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
